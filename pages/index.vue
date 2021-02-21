bcf48820ac5ab8075c5ab4a5d25dd0bbfeacc26f
yokota0211 <yokota.sist@gmail.com>
2021-02-21 15:59:35 +0900

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <p>{{ message }}</p>
+    <router-link to="/price">Price Page test</router-link>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function(){
+    return{
+      message: 'Hell222o!'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/price.vue
@@ -0,0 +1,7 @@
+<template>
+    <div class="container">
+        <h1>Price Page</h1>
+         <router-link to="/">Top Page</router-link>
+    </div>
+
+</template>
